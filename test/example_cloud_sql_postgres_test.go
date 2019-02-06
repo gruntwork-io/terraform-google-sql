8ba64b8e8eb1049d20a1eaf78aed9411b9fe598e
Petri Autero <petri@gruntwork.io>
2019-02-06 16:39:41 +0200

    Remove unnecessary comment

--- test/example_cloud_sql_postgres_test.go
+++ test/example_cloud_sql_postgres_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// A basic sanity check of the MySQL example that just deploys and undeploys it to make sure there are no errors in
-// the templates
-// TODO: try to actually connect to the RDS DBs and check they are working
 func TestCloudSQLPostgres(t *testing.T) {
 	t.Parallel()
 
