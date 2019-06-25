925924a298066fcd5cc5dbd7c764aa6b63fa443f
Rob Morgan <robbym@gmail.com>
2019-06-25 18:08:10 +0200

    fix code

--- examples/postgres-private-ip/main.tf
+++ examples/postgres-private-ip/main.tf
@@ -40,25 +40,25 @@ locals {
 # Simple network, auto-creates subnetworks
 resource "google_compute_network" "private_network" {
   provider = "google-beta"
-  name     = "${local.private_network_name}"
+  name     = local.private_network_name
 }
 
 # Reserve global internal address range for the peering
 resource "google_compute_global_address" "private_ip_address" {
   provider      = "google-beta"
-  name          = "${local.private_ip_name}"
+  name          = local.private_ip_name
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = "${google_compute_network.private_network.self_link}"
+  network       = google_compute_network.private_network.self_link
 }
 
 # Establish VPC network peering connection using the reserved address range
 resource "google_service_networking_connection" "private_vpc_connection" {
   provider                = "google-beta"
-  network                 = "${google_compute_network.private_network.self_link}"
+  network                 = google_compute_network.private_network.self_link
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = ["${google_compute_global_address.private_ip_address.name}"]
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
 # ------------------------------------------------------------------------------
@@ -71,28 +71,28 @@ module "postgres" {
   # source = "github.com/gruntwork-io/terraform-google-sql.git//modules/cloud-sql?ref=v0.2.0"
   source = "../../modules/cloud-sql"
 
-  project = "${var.project}"
-  region  = "${var.region}"
-  name    = "${local.instance_name}"
-  db_name = "${var.db_name}"
+  project = var.project
+  region  = var.region
+  name    = local.instance_name
+  db_name = var.db_name
 
-  engine       = "${var.postgres_version}"
-  machine_type = "${var.machine_type}"
+  engine       = var.postgres_version
+  machine_type = var.machine_type
 
   # These together will construct the master_user privileges, i.e.
   # 'master_user_name'@'master_user_host' IDENTIFIED BY 'master_user_password'.
   # These should typically be set as the environment variable TF_VAR_master_user_password, etc.
   # so you don't check these into source control."
-  master_user_password = "${var.master_user_password}"
+  master_user_password = var.master_user_password
 
-  master_user_name = "${var.master_user_name}"
+  master_user_name = var.master_user_name
   master_user_host = "%"
 
   # Pass the private network link to the module
-  private_network = "${google_compute_network.private_network.self_link}"
+  private_network = google_compute_network.private_network.self_link
 
   # Wait for the vpc connection to complete
-  dependencies = ["${google_service_networking_connection.private_vpc_connection.network}"]
+  dependencies = [google_service_networking_connection.private_vpc_connection.network]
 
   custom_labels = {
     test-id = "postgres-private-ip-example"
