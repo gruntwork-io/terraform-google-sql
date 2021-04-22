7a02a34de8a66cdae969b8695e862af1c28a3ea9
Rob Morgan <robbym@gmail.com>
2021-04-22 08:10:13 +0800

    Terraform v0.14 update (#60)

--- main.tf
+++ main.tf
@@ -7,16 +7,22 @@
 # ------------------------------------------------------------------------------
 
 provider "google-beta" {
-  version = "~> 3.57.0"
   project = var.project
   region  = var.region
 }
 
 terraform {
-  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.13.x code.
+  # forwards compatible with 0.14.x code.
   required_version = ">= 0.12.26"
+
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.57.0"
+    }
+  }
 }
 
 # ------------------------------------------------------------------------------
